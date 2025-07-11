19ff37cb4d6c293cf91be5c945a3056e432d292e
Arnav Srivastava <arnavsrivastava214@gmail.com>
2025-07-11 11:45:40 +0530

    missed code commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -132,10 +132,6 @@ export const routes: Routes = [
         path: 'notifications',
         loadComponent: () => import("./components/super-admin/manage-features/notification/notification.component").then(m => m.NotificationComponent),
       },
-      {
-        path: 'practice',
-        loadComponent: () => import("./components/super-admin/practicevalidation/practicevalidation.component").then(m => m.PracticevalidationComponent),
-      },
    
     ]
 
