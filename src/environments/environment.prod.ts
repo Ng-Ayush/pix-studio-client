91f5413b593451b39250ea6357ff1ad1fcd84da1
Ng-Ayush <ayushsrivastava1011@gmail.com>
2025-07-18 10:52:07 +0530

    razorpay integration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: `https://api.mystudioitsolutions.com`,
+  razorpay_key:"rzp_test_jODMqQ414J8DOi",
   firebaseConfig : {
     apiKey: "AIzaSyBH-L41Eu4UImmKAnBu-L3db30p_fwNfEs",
     authDomain: "surajproductions-3f28b.firebaseapp.com",
