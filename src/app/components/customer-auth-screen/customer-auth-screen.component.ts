19fbad6c5e65cd3548cb0768a79beaf7357cd2fd
Ng-Ayush <ayushsrivastava1011@gmail.com>
2025-08-06 11:47:07 +0530

    customer auth process changed

--- src/app/components/customer-auth-screen/customer-auth-screen.component.ts
+++ src/app/components/customer-auth-screen/customer-auth-screen.component.ts
@@ -9,46 +9,53 @@ import { AdminService } from '../../services/admin.service';
 @Component({
   selector: 'app-customer-auth-screen',
   standalone: true,
-  imports: [CommonModule,FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './customer-auth-screen.component.html',
   styleUrl: './customer-auth-screen.component.scss'
 })
 export class CustomerAuthScreenComponent {
 
-  uniqueCode:any= null;
-  loader:boolean=false;
-  userData:any={};
-  userId:any='';
+  uniqueCode: any = null;
+  loader: boolean = false;
+  userData: any = {};
+  userId: any = '';
 
-  constructor(private router:Router,private customerService:CustomerService,private admin:AdminService, private alert:AlertService,private route:ActivatedRoute){
-      this.route.queryParams.subscribe((params:any) => {
-        if(params['studio-id']){
-            this.userId = params['studio-id'];
-            this.getUserData();
-        }
-      })
-  } 
+  constructor(private router: Router, private customerService: CustomerService, private admin: AdminService, private alert: AlertService, private route: ActivatedRoute) {
+    this.route.queryParams.subscribe((params: any) => {
+      if (params['studio-id']) {
+        this.userId = params['studio-id'];
+        this.getUserData();
+      }
+    })
+  }
 
-  getUserData(){
-    this.admin.getUsersByCurrentId(this.userId,(res:any)=>{
+  getUserData() {
+    this.admin.getUsersByCurrentId(this.userId, (res: any) => {
       this.userData = res;
     })
   }
 
 
-  verifyCode(){
-    this.loader =true;
-    this.customerService.verifyUniqueCode({code:this.uniqueCode},(res:any)=>{
-      if(res.status == 200 && !res.is_event_submitted){
+  verifyCode() {
+    this.loader = true;
+    this.customerService.verifyUniqueCode({ code: this.uniqueCode }, (res: any) => {
+      if (res.status == 200 && !res.is_event_submitted) {
         this.loader = false;
-        localStorage.setItem("uniqueCode",this.uniqueCode);
-        localStorage.setItem("userData",JSON.stringify(this.userData));
+        localStorage.setItem("uniqueCode", this.uniqueCode);
+        localStorage.setItem("userData", JSON.stringify(this.userData));
         this.router.navigate(['/selection/folder-listing-screen']);
-      }else if(res.is_event_submitted){
+      } else if (res.is_event_submitted) {
         this.loader = false;
         this.alert.info("Event already submitted");
       }
     })
+  }
+
+  checkMaxLength(event: any) {
+    let val = event.target.value.replace(/\D/g, ''); // Remove non-digits
+    if (val.length > 5) val = val.substring(0, 6);
+    event.target.value = val;
+    this.uniqueCode = val;
   }
 
 }
