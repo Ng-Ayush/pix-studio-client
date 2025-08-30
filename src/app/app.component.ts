0578db3b7048996aac39303dfb322784019bc02a
Ng-Ayush <ayushsrivastava1011@gmail.com>
2025-08-30 10:34:30 +0530

    update button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { LoaderComponent } from './components/shared/loader/loader.component';
 import { UploadImageProgressComponent } from './shared/upload-image-progress/upload-image-progress.component';
 import { DeleteImageProcessComponent } from "./shared/delete-image-process/delete-image-process.component";
 import { CommonModule } from '@angular/common';
+declare var window: any;
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoaderComponent,CommonModule, UploadImageProgressComponent, DeleteImageProcessComponent],
+  imports: [RouterOutlet, LoaderComponent, CommonModule, UploadImageProgressComponent, DeleteImageProcessComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -17,11 +18,35 @@ export class AppComponent {
   title = 'pix-studio-pro';
   isLoggedIn: boolean = false;
   showLoader: boolean = true;
-
+  hasDesktopUpdated: boolean = false;
+  progress = 0;
+  interval: any = '';
   constructor(private auth: AuthService) {
     this.isLoggedIn = this.auth.isAuthenticated();
     setTimeout(() => {
       this.showLoader = false;
     }, 1500);
+  }
+
+  updateDesktopApp() {
+    if ((window as any).electronAPI) {
+      (window as any).electronAPI.updateApp();
+      this.hasDesktopUpdated=false;
+    }
+  }
+
+  updateProgress() {
+    const progressBar:any = document.getElementById('progressBar');
+
+    this.interval = setInterval(() => {
+      if (this.progress < 100) {
+        this.progress += 1;
+        progressBar.style.width = this.progress + '%';
+        progressBar.textContent = this.progress + '%';
+      } else {
+        clearInterval(this.interval);
+        this.updateDesktopApp();
+      }
+    }, 50); // 50ms per percent = ~5 seconds total
   }
 }
