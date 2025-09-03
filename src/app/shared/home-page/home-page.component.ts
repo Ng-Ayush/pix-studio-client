bdb1f050e339bf9eebf4b021fdc4807bc0e979c5
Ng-Ayush <ayushsrivastava1011@gmail.com>
2025-09-03 18:14:28 +0530

    directdownload

--- src/app/shared/home-page/home-page.component.ts
+++ src/app/shared/home-page/home-page.component.ts
@@ -14,28 +14,23 @@ export class HomePageComponent {
 
   router: any = inject(Router);
   constructor(private alert: AlertService) { }
-  dekstopUrl: any = 'https://drive.google.com/file/d/1PzMyTJXP8ucb98mnZOp7c489DQ-cwKZ5/view?usp=sharing';
 
   goToLogin() {
     this.router.navigate(['/login']);
   }
 
   downloadDesktopApp() {
-    const fileId = this.extractFileId(this.dekstopUrl);
-    const downloadUrl = `https://drive.google.com/uc?export=download&id=${fileId}`;
+    const downloadUrl = `https://firebasestorage.googleapis.com/v0/b/surajproductions-3f28b.firebasestorage.app/o/default%20assets%2FMy%20Studio%20Desktop%20Setup%201.0.0.exe?alt=media&token=62bd1eb9-a550-4c25-81ce-fdcc7b9475b9`;
     try {
       setTimeout(() => {
         const a = document.createElement('a');
         a.href = downloadUrl;
-        a.download = '';
         a.target = '_blank';
-        document.body.appendChild(a);
+        a.setAttribute('download', '');
         a.click();
-        document.body.removeChild(a);
       }, 300);
-    }
-    catch (error: any) {
-      this.alert.error("Error in downloading the application")
+    } catch (error: any) {
+      this.alert.error("Error in downloading the application");
     }
   }
 
