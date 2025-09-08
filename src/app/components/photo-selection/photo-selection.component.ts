b8b15ce311177761065092638fca4ff3b799d4cd
Ng-Ayush <ayushsrivastava1011@gmail.com>
2025-09-08 21:24:00 +0530

    app store and weblink updated in event selection

--- src/app/components/photo-selection/photo-selection.component.ts
+++ src/app/components/photo-selection/photo-selection.component.ts
@@ -159,7 +159,7 @@ export class PhotoSelectionComponent {
 
 
   copyMessage(event: any) {
-    const message = `Dear ${event.customer_name},\nYour event ${event.event_name} is ready for photo selection. Your event code is ${event.customer_unique_id} and you can select photos from\n\nWebsite : ${window.location.origin}/login \n\nApp : ${window.location.origin}/download-apk \n\nRegards ${this.userData?.studio_name}`
+    const message = `Dear *${event.customer_name}*,\nYour event *${event.event_name}* is ready for photo selection. Your event code is *${event.customer_unique_id}* and you can select photos from\n\n*Website* : ${window.location.origin}/login \n\n*App* : https://play.google.com/store/apps/details?id=app.mystudio&pcampaignid=web_share/ \n\nRegards *${this.userData?.studio_name}*`
     navigator.clipboard.writeText(message).then(() => {
       this.alert.success('Message copied to clipboard');
     }).catch(err => {
