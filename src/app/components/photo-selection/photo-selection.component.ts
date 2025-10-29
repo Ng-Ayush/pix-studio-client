302e46fe6fffd6899cf3cc18982386b28b215c4f
Ayush Srivastava <ayushsrivastava1011@gmail.com>
2025-10-29 18:01:00 +0530

    app store link added

--- src/app/components/photo-selection/photo-selection.component.ts
+++ src/app/components/photo-selection/photo-selection.component.ts
@@ -154,7 +154,7 @@ export class PhotoSelectionComponent {
   }
 
   copyMessage(event: any) {
-    const message = `Dear *${event.customer_name}*,\nYour event *${event.event_name}* is ready for photo selection. Your event code is *${event.customer_unique_id}* and you can select photos from\n\n*Website* : ${window.location.origin}/login \n\n*App* : https://play.google.com/store/apps/details?id=app.mystudio&pcampaignid=web_share/ \n\nRegards *${this.userData?.studio_name}*`
+    const message = `Dear *${event.customer_name}*,\nYour event *${event.event_name}* is ready for photo selection. Your event code is *${event.customer_unique_id}* and you can select photos from...\n\n*Website* : ${window.location.origin}/login \n*Playstore* : https://play.google.com/store/apps/details?id=app.mystudio&pcampaignid=web_share/ \n*Appstore* : https://apps.apple.com/us/app/my-studio/id6740578872 \n\nRegards *${this.userData?.studio_name}*`
     navigator.clipboard.writeText(message).then(() => {
       this.alert.success('Message copied to clipboard');
     }).catch(err => {
