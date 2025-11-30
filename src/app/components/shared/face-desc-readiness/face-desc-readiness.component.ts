49def49aed73ba7d11b30cf4b1e5f5bd52147739
Ayush Srivastava <ayushsrivastava1011@gmail.com>
2025-11-30 13:38:20 +0530

    url changed

--- src/app/components/shared/face-desc-readiness/face-desc-readiness.component.ts
+++ src/app/components/shared/face-desc-readiness/face-desc-readiness.component.ts
@@ -85,7 +85,7 @@ export class FaceDescReadinessComponent {
     const key = `face_process_${this.event.event_id}`;
     const userStarted = localStorage.getItem(key) === 'started';
 
-    const url = `${environment.apiUrl}/check_status/${this.event.event_name.split(' ').join('_')}_${this.event.event_id}`;
+    const url = `http://157.173.221.163:8888/check_status/${this.event.event_name.split(' ').join('_')}_${this.event.event_id}`;
 
     this.http.get(url).subscribe(
       (res: any) => {
