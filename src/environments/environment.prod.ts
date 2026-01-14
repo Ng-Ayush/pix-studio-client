db0576edcf46cb640e534d45c75f99cbe43b5d60
Ayush Srivastava <ayushsrivastava1011@gmail.com>
2026-01-14 23:27:51 +0530

    http only

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   // apiUrl: `https://api.mystudioitsolutions.com`, //real live
-  apiUrl: `https://103.67.239.58`,
+  apiUrl: `http://103.67.239.58`,
   razorpay_key:"rzp_live_R6S9KpHqJSUEdA",
   firebaseConfig : {
     apiKey: "AIzaSyBH-L41Eu4UImmKAnBu-L3db30p_fwNfEs",
