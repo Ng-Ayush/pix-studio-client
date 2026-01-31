c67974cf72f64bbf289ee5d72a87929db7198701
Ayush Srivastava <ayushsrivastava1011@gmail.com>
2026-01-31 20:04:30 +0530

    batches speed improved

--- src/app/services/upload-img-background.service.ts
+++ src/app/services/upload-img-background.service.ts
@@ -171,7 +171,7 @@ export class UploadImgBackgroundService {
       progressPercentage$.next(0);
       isUploading$.next(true);
 
-      const BATCH_SIZE = 6;
+      const BATCH_SIZE = 10
       const COMPRESSION_CONCURRENCY = 6; // Compress 10 images in parallel
       const options = {
         maxSizeMB: 0.1, // 100KB
--- src/app/services/upload-img-bg-ai.service.ts
+++ src/app/services/upload-img-bg-ai.service.ts
@@ -211,7 +211,7 @@ export class UploadImgBackgroundAiService {
       this.progressPercentage$.next(0);
       this.isUploading$.next(true);
 
-      const BATCH_SIZE = 6;
+      const BATCH_SIZE = 10;
       const COMPRESSION_CONCURRENCY = 6; // Compress 10 images in parallel
 
       try {
