35902f141dbbde6f976a68aa09bae4b01132f669
Ayush Srivastava <ayushsrivastava1011@gmail.com>
2026-05-04 17:49:23 +0530

    limit changed to 5 to 10

--- src/app/components/ai-upload/ai-upload.component.ts
+++ src/app/components/ai-upload/ai-upload.component.ts
@@ -65,7 +65,7 @@ export class AiUploadComponent {
   showThankyouMessage: boolean = false;
 
   currentPage = 1;
-  limit = 5;
+  limit = 10;
   loading = false;
   hasMore = true;
   private scrollTimeout: any;
